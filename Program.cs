17f80b768310c1eb1cc1b06a1b8191b9515a564e
Abhiram13 <abhiram131869@gmail.com>
2020-12-02 20:47:47 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CRM {
+   class Program {
+      static void Main(string[] args) {
+         Console.WriteLine("Welcome to CRM");
+      }
+   }
+}
---
+++ index.cs
@@ -0,0 +1,22 @@
+using System;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Net;
+
+namespace CRM {
+   class Server {
+      public static HttpListener http = new HttpListener();
+      public static void start() {
+         http.Prefixes.Add("http://localhost:2001/");
+         http.Start();
+         Console.WriteLine("Server had Started");
+         while (true) {
+            HttpListenerContext context = http.GetContext();
+            switch (context.Request.RawUrl) {
+               case "/":
+                  break;
+            }
+         }
+      }
+   }
+}
