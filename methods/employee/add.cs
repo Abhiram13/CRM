5be799015e956b4ee3ec8187fada1815b4ddcbd5
Abhiram13 <abhiram131869@gmail.com>
2020-12-20 15:41:37 +0530

    fixed fetch all error

--- methods/employee/add.cs
+++ methods/employee/add.cs
@@ -4,15 +4,10 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace CRM
 {
-   // public class MongoObject<T> : T where T : class
-   // {
-   //    public MongoDB.Bson.ObjectId? _id { get; }
-   //    public int? __v { get; }
-   // }
-
    public class IEmployee
    {
-      public MongoDB.Bson.ObjectId? _id { get; set; }
+      [BsonIgnoreIfNull]
+      public MongoDB.Bson.ObjectId _id { get; set; } = new ObjectId();
       public int ID { get; set; } = 0;
       public string TITLE { get; set; } = "";
       public string FIRSTNAME { get; set; } = "";
@@ -27,31 +22,6 @@ namespace CRM
       public string STATE { get; set; } = "";
       public string REGION { get; set; } = "";
       public string ROLE { get; set; } = "";
-      public int? __v { get; }
-   }
-
-   public class IEmployeeMongo
-   {
-      public MongoDB.Bson.ObjectId? _id { get; }
-      public int ID { get; set; }
-      public string TITLE { get; set; }
-      public string FIRSTNAME { get; set; }
-      public string LASTNAME { get; set; }
-      public string EMAIL { get; set; }
-      public string PASSWORD { get; set; }
-      public int MOBILE { get; set; }
-      public string GENDER { get; set; }
-      public string DESIGNATION { get; set; }
-      public string LOCATION { get; set; }
-      public string BRANCH { get; set; }
-      public string STATE { get; set; }
-      public string REGION { get; set; }
-      public string ROLE { get; set; }
-      // public int? __v { get; }
-   }
-
-   public static class Employee
-   {
-
+      public int? __v { get; } = 0;
    }
 }
