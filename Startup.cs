6daf3332a9ebbef61737949837d2ea73abe10327
Abhiram13 <abhiram131869@gmail.com>
2020-12-22 21:13:08 +0530

    added config

--- Startup.cs
+++ Startup.cs
@@ -1,20 +1,15 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Microsoft.AspNetCore.Http;
 using System.Text.Json;
 using System.IO;
-using MongoDB.Driver;
 
 namespace CRM {
    public sealed class Demo {
@@ -25,12 +20,6 @@ namespace CRM {
       public void method() {
          Console.WriteLine("Routing");
       }
-   }
-
-   public static class Mongo {
-      public static string url = "";
-      public static MongoClient client = new MongoClient($"mongodb+srv://abhiramdb:abhiram13@crm-cluster.i47fm.mongodb.net/CRM-Cluster?retryWrites=true&w=majority");
-      public static IMongoDatabase database = client.GetDatabase("CRM");
    }
 
    public class Startup {
