29f5a9fe3d168ced8cda4d0909a3edd6465949f2
Abhiram13 <abhiram131869@gmail.com>
2020-12-23 20:44:11 +0530

    added product methods

--- methods/product.cs
+++ methods/product.cs
@@ -5,7 +5,31 @@ using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
 
 namespace CRM {
-   class Product {
-      
+   public class IProduct {
+      public string PRODUCT { get; set; }
+      public string COMPANY { get; set; }
+      public string TYPE { get; set; }
+   }
+
+   public class Product : JSON {
+      HttpContext context;
+      Task<IProduct> product;
+      public Product(HttpContext Context) {
+         context = Context;
+         product = Deserilise<IProduct>(Context);
+      }
+
+      public async Task<IProduct[]> fetchAllProducts() {
+         string listOfProducts = await new Database<IProduct>("product").FetchAll();
+         return DeserializeObject<IProduct[]>(listOfProducts);
+      }
+
+      private async void isProductExist() {
+         IProduct product = await this.product;
+      }
+
+      public async void Add() {
+         new Database<IProduct>("product").Insert(await this.product);
+      }
    }
 }
