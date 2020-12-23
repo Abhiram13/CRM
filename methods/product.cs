7678f6022b69c160876b72e6f61b01ba68b97f21
Abhiram13 <abhiram131869@gmail.com>
2020-12-23 20:28:52 +0530

    added product class

---
+++ methods/product.cs
@@ -0,0 +1,11 @@
+using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace CRM {
+   class Product {
+      
+   }
+}
