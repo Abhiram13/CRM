65343b2bc3303ac9d1e19f4154e0bf63c74b0d49
Abhiram13 <abhiram131869@gmail.com>
2020-12-23 21:29:11 +0530

    added life insurance transaction schema object

--- methods/transactions/lifeInsurance.cs
+++ methods/transactions/lifeInsurance.cs
@@ -5,6 +5,29 @@ using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
 
 namespace CRM {
-   class ILifeTransaction {
+   public class ILifeTransaction : IMongoObject {
+      public string MOBILE { get; set; }
+      public string AADHAAR { get; set; }
+      public string ACCOUNT { get; set; }
+      public string BANK { get; set; }
+      public string PLAN { get; set; }
+      public int TERM { get; set; }
+      public string COMPANY { get; set; }
+      public int PREMIUM_PAYMENT_TERM { get; set; }
+      public string GROSS { get; set; }
+      public string NET { get; set; }
+      public string ENTRY_DATE { get; set; }
+      public string REVENUE { get; set; }
+   }
+
+   public class LifeTransaction : JSON {
+      HttpContext context;
+      Task<ILifeTransaction> transaction;
+      public LifeTransaction(HttpContext Context) {
+         context = Context;
+         transaction = Deserilise<ILifeTransaction>(Context);
+      }
+
+      public void Add() {}
    }
 }
