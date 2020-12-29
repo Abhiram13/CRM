2a6ebb97345a66d51d76a3c4d64da570f4ef29e1
Abhiram13 <abhiram131869@gmail.com>
2020-12-29 20:04:50 +0530

    adding transaction will now check for customers

--- methods/transaction.cs
+++ methods/transaction.cs
@@ -3,16 +3,29 @@ using System.Threading.Tasks;
 
 namespace CRM {
    public class Transaction<TransactionType> : JSON {
-      HttpContext Context;
-      Task<TransactionType> transaction;
-      string name;
+      private HttpContext Context;
+      private Task<TransactionType> transaction;
+      private string name;
       public Transaction(HttpContext context, string transactionName) {
          Context = context;
          transaction = Deserilise<TransactionType>(context);
          name = transactionName;
       }
 
+      private async Task<ICustomer> checkIfCustomerExist() {
+         ICustomer[] customers = await new Customer(this.Context).fetchAllCustomers();
+         var MOBILE = typeof(TransactionType).GetProperty("MOBILE").GetValue(await this.transaction);
+         var AADHAAR = typeof(TransactionType).GetProperty("AADHAAR").GetValue(await this.transaction);
+
+         ICustomer c = System.Array.Find<ICustomer>(customers, cust => cust.AADHAAR == (long)AADHAAR && cust.MOBILE == (long)MOBILE);
+         return c;
+      }
+
       public async Task<string> Add() {
+         if (await this.checkIfCustomerExist() == null) {
+            return "Customer not Found";
+         }
+
          new Database<TransactionType>(this.name).Insert(await this.transaction);
          return "Transaction successfully added";
       }
