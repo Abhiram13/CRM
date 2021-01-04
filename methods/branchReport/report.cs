caf9f3d220cfed66b4ca6772e17de599b95f2d82
Abhiram13 <abhiram131869@gmail.com>
2021-01-04 20:21:38 +0530

    branch reports can now be fetched

--- methods/branchReport/report.cs
+++ methods/branchReport/report.cs
@@ -12,6 +12,15 @@ namespace CRM {
       public DateTime START_DATE { get; set; }
    }
 
+   public class IReportObject : ILifeTransaction {
+      public string FIRSTNAME { get; set; }
+      public string LASTNAME { get; set; }
+      public string EMAIL { get; set; }
+      public DateTime BIRTHDATE { get; set; }
+      public string LOCATION { get; set; }
+      public string BRANCH { get; set; }
+   }
+
    public class Report<TransactionType> : JSON {
       Task<IReport> context;
       string TransactionName;
@@ -46,51 +55,61 @@ namespace CRM {
 
       private async Task<ILifeTransaction[]> filterTransaction() {
          IReport report = await this.context;
-         // var gte_start = Builders<TransactionType>.Filter
-         //    .Gte(transaction => (DateTime) typeof(TransactionType).GetProperty("ENTRY_DATE").GetValue(transaction, null), DateTime.Parse(report.START_DATE.ToString()));
-
-         // var lte_end = Builders<TransactionType>.Filter
-         //    .Lte(transaction => (DateTime) typeof(TransactionType).GetProperty("ENTRY_DATE").GetValue(transaction, null), DateTime.Parse(report.END_DATE.ToString()));
-
-         // List<TransactionType> list = Mongo.database.GetCollection<TransactionType>(this.TransactionName).Find<TransactionType>(gte_start & lte_end).ToList();
-
-         // foreach (TransactionType t in list) {
-         //    Console.WriteLine(t.GetType().GetProperty("ENTRY_DATE").GetValue(t));
-         // }
-
-         // return list.ToArray();
-
          var gte_start = Builders<ILifeTransaction>.Filter
             .Gte(transaction => transaction.ENTRY_DATE, DateTime.Parse(report.START_DATE.ToString()));
 
          var lte_end = Builders<ILifeTransaction>.Filter
-         .Lte(transaction => transaction.ENTRY_DATE, DateTime.Parse(report.END_DATE.ToString()));
+            .Lte(transaction => transaction.ENTRY_DATE, DateTime.Parse(report.END_DATE.ToString()));
 
          List<ILifeTransaction> list = Mongo.database.GetCollection<ILifeTransaction>("life_insurance").Find<ILifeTransaction>(gte_start & lte_end).ToList();
 
          return list.ToArray();
       }
 
-      private async void fetchReports() {
+      private async Task<IReportObject[]> fetchReports() {
          ILifeTransaction[] transactions = await this.filterTransaction();
          ICustomer[] customers = await this.filterCustomers();
+         List<IReportObject> reports = new List<IReportObject>();
 
          for (int cIndex = 0; cIndex < customers.Length; cIndex++) {
-            long? MOBILE = customers[cIndex].MOBILE;
+            ICustomer customer = customers[cIndex];
+            long? MOBILE = customer.MOBILE;
 
             for (int tIndex = 0; tIndex < transactions.Length; tIndex++) {
-               long tMobile = (long)transactions[tIndex].GetType().GetProperty("MOBILE").GetValue(transactions[tIndex]);
+               ILifeTransaction transaction = transactions[tIndex];
+               long tMobile = transaction.MOBILE;
 
                if (tMobile == MOBILE) {
-                  Console.WriteLine(transactions[tIndex]);
+                  reports.Add(new IReportObject() {
+                     AADHAAR = (long)customer.AADHAAR,
+                     ACCOUNT = transaction.ACCOUNT,
+                     BANK = transaction.BANK,
+                     BIRTHDATE = (DateTime)customer.BIRTHDATE,
+                     BRANCH = customer.BRANCH,
+                     COMPANY = transaction.COMPANY,
+                     EMAIL = customer.EMAIL,
+                     ENTRY_DATE = transaction.ENTRY_DATE,
+                     FIRSTNAME = customer.FIRSTNAME,
+                     GROSS = transaction.GROSS,
+                     LASTNAME = customer.LASTNAME,
+                     LOCATION = customer.LOCATION,
+                     MOBILE = (long)customer.MOBILE,
+                     NET = transaction.NET,
+                     PLAN = transaction.PLAN,
+                     PREMIUM_PAYMENT_TERM = transaction.PREMIUM_PAYMENT_TERM,
+                     REVENUE = transaction.REVENUE,
+                     TERM = transaction.TERM,
+                  });
                }
             }
          }
+
+         return reports.ToArray();
       }
 
       public async Task<string> customers() {
-         this.fetchReports();
-         return Serialize<ICustomer[]>(await this.filterCustomers());
+         return Serialize<IReportObject[]>(await this.fetchReports());
+         // return Serialize<ICustomer[]>(await this.filterCustomers());
       }
    }
 }
