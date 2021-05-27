209544c3336b62ca4fab8e862a44c5592d20b5d1
Abhiram13 <abhiram131869@gmail.com>
2021-05-27 15:48:18 +0530

    commit

--- database.cs
+++ database.cs
@@ -9,13 +9,9 @@ namespace CRM {
    public class Database<DocumentType> {
       private IMongoCollection<DocumentType> collection;
 
-      public Database(string CollectionName) {
-         collection = Mongo.database.GetCollection<DocumentType>(CollectionName);
-      }
+      public Database(string CollectionName) => collection = Mongo.database.GetCollection<DocumentType>(CollectionName);
 
-      public void Insert(DocumentType doc) {
-         collection.InsertOne(doc);
-      }
+      public void Insert(DocumentType doc) => collection.InsertOne(doc);
 
       public async Task<string> FetchAll() {
          List<DocumentType> list = await collection.Find<DocumentType>(new BsonDocument()).ToListAsync<DocumentType>();
