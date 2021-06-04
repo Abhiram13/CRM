fe8699781a1b1963c0c761233f31fc1c8e007047
Abhiram13 <abhiram131869@gmail.com>
2021-06-04 12:55:26 +0530

    commit

---
+++ controllers/fixeddeposit/FixedDeposit.cs
@@ -0,0 +1,30 @@
+using System;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using Models;
+using CRM;
+using Models.TransactionsRequestBody;
+using CustomerManagement;
+using EmployeeManagement;
+
+namespace TransactionManagement {
+   public class FixedDepositTransactionController : TransactionController {
+      private Task<FixedDepositBody> transaction;
+
+      public FixedDepositTransactionController(HttpContext context) {
+         this.transaction = JSONObject.Deserilise<FixedDepositBody>(context);
+      }
+
+      public async Task<string> Add() {
+         FixedDepositBody trans = await this.transaction;
+         TransactionVerification<FixedDepositBody> details = new TransactionVerification<FixedDepositBody>() {
+            document = trans,
+            isCustomerExist = await CustomerController.IsCustomerExist(trans.MOBILE),
+            isEmployeeExist = await EmployeeController.IsEmployeeExist(trans.MANAGER),
+            table = Table.lifeInsurance,
+         };
+
+         return AddTransaction<FixedDepositBody>(details);
+      }
+   }
+}
