44bab34f0d129e5e49fe7362c1bce8c508cc584f
Abhiram13 <abhiram131869@gmail.com>
2021-08-06 19:38:19 +0530

    commit

--- controllers/HomeController.cs
+++ controllers/HomeController.cs
@@ -10,19 +10,19 @@ namespace Controllers {
          [HttpGet]
          [Route("states")]
          public string[] States() {
-            return Services.ApiManagement.Api.fetchStates();
+            return Services.ApiManagement.States.fetchStates();
          }
 
          [HttpGet]
          [Route("roles")]
          public string[] Roles() {
-            return Services.ApiManagement.Api.fetchRoles();
+            return Services.ApiManagement.Roles.fetchRoles();
          }
 
          [HttpGet]
          [Route("locations")]
          public string[] Locations() {
-            return Services.ApiManagement.Api.fetchLocations();
+            return Services.ApiManagement.Locations.fetchLocations();
          }
       }
    }
--- services/employee/FetchAllEmployees.cs
+++ services/employee/FetchAllEmployees.cs
@@ -2,12 +2,15 @@ using Models;
 using CRM;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Http;
+using Services.DatabaseManagement;
+using System;
 
 namespace Services {
    namespace EmployeeManagement {
-      public partial class EmployeeService : Services<Employee> {         
-         public List<EmployeeResponseBody> FetchAllEmployees(HttpRequest request) {
-            List<Employee> employeesList = document.FetchAll();
+      public partial class EmployeeService {         
+         public static List<EmployeeResponseBody> FetchAllEmployees(HttpRequest request) {
+				Document<Employee> document = new Document<Employee>(Table.employee);
+				List<Employee> employeesList = document.FetchAll();
             List<EmployeeResponseBody> response = new List<EmployeeResponseBody>();
 
             foreach (Employee employee in employeesList) {
