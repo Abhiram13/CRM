4a7f225c5885b30eee65bc7f40fe6fd039e111dc
Abhiram13 <abhiram131869@gmail.com>
2021-08-13 20:37:10 +0530

    commit

--- attribute.cs
+++ attribute.cs
@@ -1,13 +1,30 @@
 using System;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
 
 namespace System {
    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
    public class ResponseHeadersAttribute : ActionFilterAttribute {
-		public override void OnResultExecuting(ResultExecutingContext context) {
-			context.HttpContext.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
-			base.OnResultExecuting(context);
+		// public override void OnResultExecuting(ResultExecutingContext context) {
+		// 	HttpResponse response = context.HttpContext.Response;
+		// 	// string jsonString = "Response already sent";
+		// 	// byte[] data = Encoding.UTF8.GetBytes(jsonString);
+		// 	response.Headers.Add("Access-Control-Allow-Credentials", "true");
+		// 	// response.Body.WriteAsync(data, 0, data.Length);
+		// 	base.OnResultExecuting(context);
+		// }
+
+		public override void OnActionExecuting(ActionExecutingContext context) {
+			HttpResponse response = context.HttpContext.Response;
+			response.Headers.Add("Access-Control-Allow-Credentials", "true");
+			UnauthorizedObjectResult result = new UnauthorizedObjectResult("asdgasdasgd") {
+				StatusCode = 200,
+			};
+
+			context.Result = result;
+			base.OnActionExecuting(context);
 		}
 	}
 }
