0de0532bfcfeb98b5ea14373e8ef32d446443f64
Abhiram13 <abhiram131869@gmail.com>
2021-08-13 21:31:09 +0530

    commit

---
+++ Roles.cs
@@ -0,0 +1,18 @@
+namespace System {
+   public sealed class RoleType {
+		public const string Admin = "Admin";
+		public const string ZonalManager = "Zonal Manager";
+		public const string NationalSManager = "national Sales Manager";
+		public const string RegionalSManager = "Regional Sales Manager";
+		public const string BranchManager = "Branch Manager";
+		public const string SrRelationashipManager = "Sr.Relationship Manager";
+		public const string RelationashipManager = "Relationship Manager";
+		public const string AssistantManager = "Assistant Manager";
+		public const string TelesalesManager = "Telesales Manager";
+		public const string TeamLeader = "Team Leader";
+		public const string SrTeamLeader = "Sr.Team Leader";
+		public const string Backend = "Backend Operations";
+		public const string AccountManager = "Account Manager";
+		public const string HR = "HR";
+	}
+}
--- attribute.cs
+++ attribute.cs
@@ -7,23 +7,14 @@ using System.Text;
 namespace System {
    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
    public class ResponseHeadersAttribute : ActionFilterAttribute {
-		// public override void OnResultExecuting(ResultExecutingContext context) {
-		// 	HttpResponse response = context.HttpContext.Response;
-		// 	// string jsonString = "Response already sent";
-		// 	// byte[] data = Encoding.UTF8.GetBytes(jsonString);
-		// 	response.Headers.Add("Access-Control-Allow-Credentials", "true");
-		// 	// response.Body.WriteAsync(data, 0, data.Length);
-		// 	base.OnResultExecuting(context);
-		// }
-
 		public override void OnActionExecuting(ActionExecutingContext context) {
 			HttpResponse response = context.HttpContext.Response;
-			response.Headers.Add("Access-Control-Allow-Credentials", "true");
-			UnauthorizedObjectResult result = new UnauthorizedObjectResult("asdgasdasgd") {
-				StatusCode = 200,
-			};
+			response.Headers.Add("Access-Control-Allow-Credentials", "true");			
+			// UnauthorizedObjectResult result = new UnauthorizedObjectResult("asdgasdasgd") {
+			// 	StatusCode = 200,
+			// };
 
-			context.Result = result;
+			// context.Result = result;
 			base.OnActionExecuting(context);
 		}
 	}
