be97dc54bc850ee18aa7648836f7ad0e77d37ae8
Abhiram13 <abhiram131869@gmail.com>
2021-08-15 14:42:17 +0530

    commit

--- attribute.cs
+++ attribute.cs
@@ -16,6 +16,7 @@ namespace System {
 		public override void OnActionExecuting(ActionExecutingContext context) {
 			HttpResponse response = context.HttpContext.Response;
 			response.Headers.Add("Access-Control-Allow-Credentials", "true");
+			response.Headers.Add("Access-Control-Allow-Origin", "http://localhost:3000/");
 			base.OnActionExecuting(context);
 		}
 	}
@@ -23,23 +24,31 @@ namespace System {
 	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
    public class RoleAuthoriseAttribute : ActionFilterAttribute {
 		private string[] roles;
+      public RoleAuthoriseAttribute() {}
       public RoleAuthoriseAttribute(string[] Roles) {
 			roles = Roles;
 		}
 
 		public override void OnActionExecuting(ActionExecutingContext context) {
-			EmployeeResponseBody loggedInEmployee = EmployeeService.fetchByCookie(context.HttpContext.Request);
+         try {
+				EmployeeResponseBody loggedInEmployee = EmployeeService.fetchByCookie(context.HttpContext.Request);
 
-			// Finds logged in employee role in the array of roles
-			// if no role matches, then employee is not authorised
-			string isRoleExist = Array.Find<string>(roles, role => role == loggedInEmployee.role);
-			if (isRoleExist == null) {
-				UnauthorizedObjectResult result = new UnauthorizedObjectResult("You are Unauthorised to access this Route") {
+				// Finds logged in employee role in the array of roles
+				// if no role matches, then employee is not authorised
+				string isRoleExist = Array.Find<string>(roles, role => role == loggedInEmployee.role);
+				if (isRoleExist == null || loggedInEmployee == null) {
+					UnauthorizedObjectResult result = new UnauthorizedObjectResult("You are Unauthorised to access this Route") {
+						StatusCode = 401,
+					};
+					context.Result = result;
+				}
+				base.OnActionExecuting(context);
+         } catch (InvalidCookieException error) {
+				UnauthorizedObjectResult result = new UnauthorizedObjectResult(error.Message) {
 					StatusCode = 401,
 				};
 				context.Result = result;
-			}
-			base.OnActionExecuting(context);
+         }
 		}
 	}
 }
