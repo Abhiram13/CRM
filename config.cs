b06267b7eff28ecefb2c19988e561bf738dfe1d8
Abhiram13 <abhiram131869@gmail.com>
2021-09-21 21:23:37 +0530

    commit

--- config.cs
+++
@@ -1,9 +0,0 @@
-using MongoDB.Driver;
-
-namespace CRM {
-   public static class Mongo {
-      public static string url = "";
-      public static MongoClient client = new MongoClient($"mongodb+srv://abhiramdb:abhiram13@crm-cluster.i47fm.mongodb.net/CRM-Cluster?retryWrites=true&w=majority");
-      public static IMongoDatabase database = client.GetDatabase("CRM");
-   }
-}
